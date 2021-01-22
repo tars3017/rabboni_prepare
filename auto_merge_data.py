6842e03a03fe173f57c1041e8599119a63a62625
pclh-dumb <64685394+pclh-dumb@users.noreply.github.com>
2021-01-22 10:19:07 +0800

    add "merge 7 csv file feature"
    usage :  import auto_merge_data
                 auto_merge_data.mergeFile()
    then it will generate a csv called "merge.csv" which contain 7 rows(packet, GyrX, GyrY, GyrZ, AccX, AccY, AccZ)
    it wiil detect filename, so don't modify the filename of raw data(like Accx.csv...)

---
+++ auto_merge_data.py
@@ -0,0 +1,59 @@
+import csv
+import os
+import numpy
+def mergeFile():
+    cur = os.getcwd()
+    final = [[]]
+    def write_in_file(file):
+        with open(filename, 'r', newline='') as f:
+            rows = csv.reader(f)
+            for row in rows:
+                final.append(row)
+                break
+
+
+
+    for filename in os.listdir(cur):        
+        if(filename.startswith("GyrX")):
+            with open(filename, 'r', newline='') as f:
+                rows = csv.reader(f)
+                packet = []
+                final.append([])
+                for row in rows:
+                    for i in range(len(row)):
+                        packet.append(i+1)
+                        final[1].append(row[i])
+                final[0] = packet
+            break
+    for filename in os.listdir(cur):
+        if(filename.startswith("GyrY")):
+            write_in_file(filename)
+            break
+    for filename in os.listdir(cur):
+        if(filename.startswith("GyrZ")):
+            write_in_file(filename)
+            break
+    for filename in os.listdir(cur):
+        if(filename.startswith("AccX")):
+            write_in_file(filename)
+            break
+    for filename in os.listdir(cur):
+        if(filename.startswith("AccY")):
+            write_in_file(filename)
+            break
+    for filename in os.listdir(cur):
+        if(filename.startswith("AccZ")):
+            write_in_file(filename)
+            break
+    # print(len(final))
+    npArray = numpy.array(final)
+    trans = npArray.T
+    transL = trans.tolist()
+    # print(transuL)
+    f = open('.\merge.csv', 'w')
+    f.close
+    for i in transL:
+        print(i)
+        with open('.\merge.csv', 'a', newline='') as f:
+            writer = csv.writer(f)
+            writer.writerow(i)
