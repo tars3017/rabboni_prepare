01a9672387b2bd6e96dfce29ad5ecae41b38bcbc
pclh-dumb <prgmpclh@gmail.com>
2021-04-14 23:22:00 +0800

    UI2 quit.png to quit.jpg

--- UI2.py
+++ UI2.py
@@ -174,7 +174,7 @@ class Ball_screen():
         self.background = pygame.transform.smoothscale(self.background,(600,500))
         self.baseball_pic = pygame.image.load("pic/baseball.png")
         self.baseball_pic = pygame.transform.smoothscale(self.baseball_pic.convert_alpha(),(30,30))
-        self.stop_button = pygame.image.load("pic/stop.png")
+        self.stop_button = pygame.image.load("pic/stop.jpg")
         self.stop_button = pygame.transform.smoothscale(self.stop_button.convert_alpha(),(200,100))
 
     def routine(self):
